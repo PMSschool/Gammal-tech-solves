6b7841ba5e2d15af8508f2099663dbd1807bee24
PMSschool <mostafahalloum85@gmail.com>
2023-09-30 18:27:20 +0200

    update the files

--- js/header.js
+++ js/header.js
@@ -22,21 +22,3 @@ window.onscroll = function (){
     scrollIcon.classList.remove("back-c");
   }
 };
-
-  let preTag = document.querySelectorAll("pre");
-
-      preTag.forEach((e) => {
-        e.addEventListener("click",() =>{
-    
-              navigator.clipboard.writeText
-                   (e.textContent);
-           });   
-      });
-
-      for(let i = 0 ; i < preTag.length ; i++){
-        let gt = document.querySelector(".gt-qu");
-        let en = document.querySelectorAll(".en");
-        en[i].onclick = () => {
-          gt.children[i+1].classList.toggle("active");
-        }
-      }
